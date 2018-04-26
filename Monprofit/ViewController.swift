7a2724b12d47dede2a2a0790395cd7c4032981b2
jarvis76 <christophermonprofit@gmail.com>
2018-04-26 19:40:07 +0200

    Initial Commit

---
+++ Monprofit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Monprofit
+//
+//  Created by Christopher MONPROFIT on 26/04/2018.
+//  Copyright © 2018 Christopher MONPROFIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
